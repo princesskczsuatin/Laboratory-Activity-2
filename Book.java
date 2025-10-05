e2c54df5cd375d8b997ea252b7d03cebb2a7ce3d
princesskczsuatin <princesskczsuatin@gmail.com>
2025-10-05 14:30:04 +0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,31 @@
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private int currentPage;
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.currentPage = 0; // default starting page
+    }
+    public void displayDetails() {
+        System.out.println("Title: " + title + 
+                           ", Author: " + author + 
+                           ", Year: " + yearPublished + 
+                           ", Current Page: " + currentPage);
+    }
+    // Method to change the current page
+    public void changePage(int page) {
+        this.currentPage = page;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getYearPublished() {
+        return yearPublished;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
